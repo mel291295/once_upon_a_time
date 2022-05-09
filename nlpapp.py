66914529c62456dbc38ad4caff71c98c9792232c
mel291295 <102040009+mel291295@users.noreply.github.com>
2022-05-09 11:30:21 +0200

    Update nlpapp.py

--- nlpapp.py
+++ nlpapp.py
@@ -1,20 +1,6 @@
-#import packages
-import streamlit as st
-import pandas as pd
-#import matplotlib.pyplot as plt
-#import matplotlib
-import seaborn as sns
-import nltk
-import textblob
-from textblob import TextBlob
-from nltk import ngrams
-from nltk.corpus import stopwords
-from nltk.sentiment import SentimentIntensityAnalyzer
-from gensim.summarization import summarize
-from summa.summarizer import summarize
+#! python3
 
-#main script
-def main():
-  #creat text box where user will write text to be analysed and extract meaningful information 
-  word = st.text_area("Enter text ", height = 120)
-  text = TextBlob(word)
+import streamlit as st
+
+st.header("Once upon a time")
+
