29c41e1503f2de2b788d34137590010bc3699360
mel291295 <102040009+mel291295@users.noreply.github.com>
2022-05-09 11:20:10 +0200

    Update nlpapp.py

--- nlpapp.py
+++ nlpapp.py
@@ -1,8 +1,8 @@
 #import packages
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
-import matplotlib
+#import matplotlib.pyplot as plt
+#import matplotlib
 import seaborn as sns
 import nltk
 import textblob
