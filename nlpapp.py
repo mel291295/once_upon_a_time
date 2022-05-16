a6a8926b08a81c7c63dbd4b778f3f5d8122db5d5
mel291295 <102040009+mel291295@users.noreply.github.com>
2022-05-16 14:38:09 +0200

    Update nlpapp.py

--- nlpapp.py
+++ nlpapp.py
@@ -1,6 +1,7 @@
 #! python3
 
-import streamlit as st
+#import necessary packages
+#import streamlit as st
 
 st.title("Once upon a time")
 st.subheader("This is a web app to summarize and translate Grimm's fairy tales")
